c33fadefa31e267687078bded88be51171e67e4e
Muhammad Bilal <bilaltheofficial@gmail.com>
2025-01-08 20:59:05 +0400

    add routing and authentication handling in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import { Route, Routes } from "react-router-dom"
+import CheckAuth from "./components/common/checkAuth";
+import AuthLayout from "./components/auth/layout";
+import { useDispatch, useSelector } from "react-redux";
+import Login from "./pages/auth/login";
+import Register from "./pages/auth/register";
+import { checkAuth } from "./store/auth-slice";
+import NotFound from "./pages/notFound/index";
+import UnAuth from "./pages/unauth/index";
+import UserLayout from "./pages/user/home";
+import Home from "./pages/user/home";
+import AdminLayout from "./components/admin/layout";
+import AdminDashboard from "./pages/admin/dashboard";
+import AdminRequests from "./pages/admin/requests";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { isAuthenticated, user, loading } = useSelector(state => state.auth)
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    const token = JSON.parse(localStorage.getItem('token'));
+    dispatch(checkAuth(token));
+  }, [dispatch]);
+
+  if (loading) return <h1>Loading...</h1>
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="flex flex-col overflow-hidden bg-white">
+      <Routes>
+        <Route path="/" element={
+          <CheckAuth isAuthenticated={isAuthenticated} user={user} />
+        } />
+        <Route path="/auth" element={
+          <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+            <AuthLayout />
+          </CheckAuth>
+        }>
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route />
+        </Route>
+        <Route path="/admin" element={
+          <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+            <AdminLayout />
+          </CheckAuth>
+        }>
+          <Route path="dashboard" element={<AdminDashboard />} />
+          <Route path="requests" element={<AdminRequests />} />
+        </Route>
+        <Route path="/home" element={
+          <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+            <UserLayout />
+          </CheckAuth>
+        }>
+          <Route path="home" element={<Home />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+        <Route path="/unauth-page" element={<UnAuth />} />
+      </Routes>
+    </div>
   )
 }
 
